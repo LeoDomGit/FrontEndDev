a8c934a3537311a077257bd4752b8346e00ecb13
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-09-28 17:53:00 +0700

    commit

--- public/admin/script/color.js
+++ public/admin/script/color.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
     addColor();
+    editColor();
 });
+function deleteColor(){
+  $(".deleteColor").click(function (e) { 
+    e.preventDefault();
+    let idColor= $(this).attr('data-id');
+  });
+}
+/**
+ * 
+ * 
+ * 
+ */
+
 function addColor(){
     $("#saveColorBtn").click(function (e) { 
         e.preventDefault(); 
@@ -82,4 +95,124 @@ function addColor(){
             });
         }
     });
+}
+/**
+ * 
+ * 
+ * 
+ */
+function editColor(){
+  $('.colorclass').click(function (e) { 
+    e.preventDefault();
+    let idColor= $(this).attr('data-id');
+    $('#submiteditColor').click(function (e) { 
+      e.preventDefault();
+      var colorname1 = $("#newColorName1").val().trim();
+      var newColorcode1 = $("#newColorpath1").val().trim();
+      if(colorname1==''){
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'error',
+          title: 'Thiếu tên màu'
+        })
+      }else if(newColorcode1==''){
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+          }
+        })
+        
+        Toast.fire({
+          icon: 'error',
+          title: 'Thiếu mã màu'
+        })
+      }else{
+        $.ajax({
+          type: "post",
+          url: "http://127.0.0.1:3000/api/editColor",
+          data: {
+            idColor:idColor,
+            colorname:colorname1,
+            newColorcode:newColorcode1,
+          },
+          dataType: "JSON",
+          success: function (response) {
+            if(response.check==true){
+              const Toast = Swal.mixin({
+                  toast: true,
+                  position: 'top-end',
+                  showConfirmButton: false,
+                  timer: 3000,
+                  timerProgressBar: true,
+                  didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                  }
+                })
+                
+                Toast.fire({
+                  icon: 'success',
+                  title: 'Đã đổi thành công'
+                }).then(()=>{
+                  window.location.reload();
+                })
+          }else{
+              if(response.message=='rejected'){
+                  const Toast = Swal.mixin({
+                      toast: true,
+                      position: 'top-end',
+                      showConfirmButton: false,
+                      timer: 3000,
+                      timerProgressBar: true,
+                      didOpen: (toast) => {
+                        toast.addEventListener('mouseenter', Swal.stopTimer)
+                        toast.addEventListener('mouseleave', Swal.resumeTimer)
+                      }
+                    })
+                    
+                    Toast.fire({
+                      icon: 'error',
+                      title: 'Dữ liệu không hợp lệ'
+                    });
+              }else if(response.message=='exist'){
+                const Toast = Swal.mixin({
+                  toast: true,
+                  position: 'top-end',
+                  showConfirmButton: false,
+                  timer: 3000,
+                  timerProgressBar: true,
+                  didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                  }
+                })
+                
+                Toast.fire({
+                  icon: 'error',
+                  title: 'Đã tồn tại màu sắc này'
+                });
+              }
+          }
+          }
+        });
+      }
+    });
+  });
 }
--- resources/views/color/index.blade.php
+++ resources/views/color/index.blade.php
@@ -2,18 +2,24 @@
 @section('title','Quản lý màu ')
 @section('main-container')
     <!-- Modal Color -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="addColorModal" tabindex="-1" aria-labelledby="addColorModalLabel" aria-hidden="true">
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Form thêm màu</h5>
+          <h5 class="modal-title" id="addColorModalLabel">Form thêm màu</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          <input type="text" placeholder="Tên màu" class="form-control" id="newColorName">
-          <input type="color" id="newColorpath" style="width:20%" class="form-control mt-2">
+            <div class="row">
+                <div class="col-10">
+                    <input type="text" placeholder="Tên màu" class="form-control" id="newColorName">
+                </div>
+                <div class="col-2">
+                    <input type="color"  id="newColorpath" style="width:50px;height:50px;border-radius:50%" class="form-control">
+                </div>
+            </div>
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
@@ -22,9 +28,37 @@
       </div>
     </div>
 </div>
+<!-- Button trigger modal -->
+  <!-- Modal -->
+  <div class="modal fade" id="editColorModal" tabindex="-1" aria-labelledby="editColorModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="editColorModalLabel">Chỉnh sửa bảng màu</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+            <div class="row">
+                <div class="col-10">
+                    <input type="text" placeholder="Tên màu" class="form-control" id="newColorName1">
+                </div>
+                <div class="col-2">
+                    <input type="color"  id="newColorpath1" style="width:50px;height:50px;border-radius:50%" class="form-control">
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+          <button type="button" id='submiteditColor' class="btn btn-primary">Lưu</button>
+        </div>
+      </div>
+    </div>
+  </div>
   {{-- End Modal Color --}}
   <div>
-    <button data-toggle="modal" data-target="#exampleModal" class="btn btn-warning">Thêm màu </button><br><br>
+    <button data-toggle="modal" data-target="#addColorModal" class="btn btn-warning">Thêm màu </button><br><br>
     <div class="">
       <table class="data-table table stripe hover nowrap">
           <thead>
@@ -39,8 +73,8 @@
           <tbody>
               @foreach ($colors as $item)
               <tr>
-                  <td class="table-plus"><p>{{$item->colorName}}</p></td>
-                  <td><p style="width:20px;height:20px;border-radius:50%;background:<?=$item->colorpicker?>" ></p></td>
+                  <td class="table-plus"><p class="colorclass" data-id="{{$item->id}}" data-toggle="modal" data-target="#editColorModal">{{$item->colorName}}</p></td>
+                  <td><p class="colorclass" data-id="{{$item->id}}" data-toggle="modal" data-target="#editColorModal" style="width:20px;height:20px;border-radius:50%;background:<?=$item->colorpicker?>" ></p></td>
                   <td><?php if($item->status==0){?>
                       <p>Đang khóa</p>
                   <?php }else{ ?>
@@ -60,10 +94,10 @@
                           <div
                               class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list"
                           >
-                              <a class="dropdown-item" href="#"
+                              <a class="dropdown-item" href="#" class="colorclass" data-id="{{$item->id}}" data-toggle="modal" data-target="#editColorModal"
                                   ><i class="dw dw-edit2"></i> Edit</a
                               >
-                              <a class="dropdown-item" href="#"
+                              <a class="dropdown-item" href="#" class="deleteColor" data-id="{{$item->id}}"
                                   ><i class="dw dw-delete-3"></i> Delete</a
                               >
                           </div>
