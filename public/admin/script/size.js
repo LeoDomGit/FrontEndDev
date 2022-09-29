632bb36064de6446fe6e584a26c2103b4acf01ca
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-09-29 11:08:05 +0700

    Update size.js

--- public/admin/script/size.js
+++ public/admin/script/size.js
@@ -11,9 +11,75 @@ function addSize(){
                 icon: 'error',
                 title: 'Oops...',
                 text: 'Thiếu tên size hoặc thông tin size!',
-              })
+              });
         }else{
-            
+            $.ajax({
+                type: "post",
+                url: "http://127.0.0.1:3000/api/addSize",
+                data: {
+                    sizeName:sizeName,
+                    sizeInfo:sizeInfo
+                },
+                dataType: "JSON",
+                success: function (response) {
+                    if(response.check==true){
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                            timerProgressBar: true,
+                            didOpen: (toast) => {
+                              toast.addEventListener('mouseenter', Swal.stopTimer)
+                              toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                          })
+                          
+                          Toast.fire({
+                            icon: 'success',
+                            title: 'Đã thêm thành công'
+                          }).then(()=>{
+                            window.location.reload();
+                          })
+                    }else{
+                        if(response.message=='rejected'){
+                            const Toast = Swal.mixin({
+                                toast: true,
+                                position: 'top-end',
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                }
+                              })
+                              
+                              Toast.fire({
+                                icon: 'error',
+                                title: 'Dữ liệu không hợp lệ'
+                              });
+                        }else if(response.message=='exist'){
+                          const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                            timerProgressBar: true,
+                            didOpen: (toast) => {
+                              toast.addEventListener('mouseenter', Swal.stopTimer)
+                              toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                          })
+                          
+                          Toast.fire({
+                            icon: 'error',
+                            title: 'Đã tồn tại thông tin size'
+                          });
+                        }
+                    }
+                }
+            });
         }
     });
 }
