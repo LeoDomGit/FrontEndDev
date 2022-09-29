1c221c19871dcea5226a00071b7ddfe5e5818484
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-09-29 12:47:15 +0700

    Update size.js

--- public/admin/script/size.js
+++ public/admin/script/size.js
@@ -1,7 +1,131 @@
 $(document).ready(function () {
     addSize();
     editSize();
+    deleteSize();
 });
+function deleteSize(){
+    $(".deleteSize").click(function (e) { 
+        e.preventDefault();
+        var idSize = $(this).attr('data-id');
+        Swal.fire({
+            title: 'Bạn muốn xóa loại size?',
+            showDenyButton: true,
+            showCancelButton: false,
+            confirmButtonText: 'Xóa',
+            denyButtonText: `Không xóa`,
+          }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+              $.ajax({
+                type: "post",
+                url: "http://127.0.0.1:3000/api/deleteSize",
+                data: {
+                    idSize:idSize
+                },
+                dataType: "JSON",
+                success: function (response) {
+                    if(response.check==true){
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                            timerProgressBar: true,
+                            didOpen: (toast) => {
+                              toast.addEventListener('mouseenter', Swal.stopTimer)
+                              toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                          })
+                          
+                          Toast.fire({
+                            icon: 'success',
+                            title: 'Đã xóa thành công'
+                          }).then(()=>{
+                            window.location.reload();
+                          })
+                    }else{
+                        if(response.message=='rejected'){
+                            const Toast = Swal.mixin({
+                                toast: true,
+                                position: 'top-end',
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                }
+                              })
+                              
+                              Toast.fire({
+                                icon: 'error',
+                                title: 'Dữ liệu không hợp lệ'
+                              });
+                        }else if(response.message=='notexists'){
+                          const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000,
+                            timerProgressBar: true,
+                            didOpen: (toast) => {
+                              toast.addEventListener('mouseenter', Swal.stopTimer)
+                              toast.addEventListener('mouseleave', Swal.resumeTimer)
+                            }
+                          })
+                          
+                          Toast.fire({
+                            icon: 'error',
+                            title: 'Không tồn tại mã size !'
+                          });
+                        }else if(response.message=='exist'){
+                            const Toast = Swal.mixin({
+                                toast: true,
+                                position: 'top-end',
+                                showConfirmButton: false,
+                                timer: 3000,
+                                timerProgressBar: true,
+                                didOpen: (toast) => {
+                                  toast.addEventListener('mouseenter', Swal.stopTimer)
+                                  toast.addEventListener('mouseleave', Swal.resumeTimer)
+                                }
+                              })
+                              
+                              Toast.fire({
+                                icon: 'error',
+                                title: 'Tồn tại sản phẩm mang mã size !'
+                              });
+                        }
+                    }
+                }
+              });
+            } else if (result.isDenied) {
+                const Toast = Swal.mixin({
+                    toast: true,
+                    position: 'top-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                      toast.addEventListener('mouseenter', Swal.stopTimer)
+                      toast.addEventListener('mouseleave', Swal.resumeTimer)
+                    }
+                  })
+                  
+                  Toast.fire({
+                    icon: 'success',
+                    title: 'OK ! Không xóa !'
+                  })
+            }
+          })
+    });
+}
+/*
+ * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * 
+ * * * * * * * * * * * * * * * 
+*/
 function editSize(){
     $(".editSize").click(function (e) { 
         e.preventDefault();
