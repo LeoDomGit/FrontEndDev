e8ab90f5aaa64970e825be91069c930963ce0896
LeoDomGit <100814585+LeoDomGit@users.noreply.github.com>
2022-10-18 14:06:23 +0700

    Commit

--- public/admin/script/storage.js
+++ public/admin/script/storage.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
-    
+    selectProduct();
 });
+function selectProduct(){
+    $('#ProductSelect').change(function (e) { 
+        e.preventDefault();
+        var idProd = $("#ProductSelect option:selected").val();
+        console.log(idProd);
+    });
+}
--- resources/views/tonkho/index.blade.php
+++ resources/views/tonkho/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layout.layout1')
 @section('title','Quản lý storages')
 @section('main-container')
-<select class="form-control col-6" name="" id="">
+<select class="form-control col-6" name="" id="ProductSelect">
     @foreach ($allProducts as $item)
-    <option value="">{{$item->prodName}}</option>        
+    <option value="{{$item->idProd}}">{{$item->prodName}}</option>        
     @endforeach
 </select>
 <script src="admin/script/jquery-3.6.1.min.js"></script>
