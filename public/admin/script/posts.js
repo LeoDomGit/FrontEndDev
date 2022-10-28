14fd4c846b356d87111c5f888c5bece775ea689c
Truong-Minh-Phung7820 <94913869+Truong-Minh-Phung7820@users.noreply.github.com>
2022-10-28 21:43:16 +0700

    update posts.js

--- public/admin/script/posts.js
+++ public/admin/script/posts.js
@@ -29,6 +29,37 @@ function ChangeToSlugAddPost(el) {
     document.getElementById('slugPostAdd').value = slug;
 }
 
+function ChangeToSlugEditPost(el) {
+    var slug;
+    var id = $(el).data("id");
+    //Lấy text từ thẻ input title 
+    slug = $(el).val();
+    slug = slug.toLowerCase();
+    //Đổi ký tự có dấu thành không dấu
+    slug = slug.replace(/á|à|ả|ạ|ã|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ/gi, 'a');
+    slug = slug.replace(/é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ/gi, 'e');
+    slug = slug.replace(/i|í|ì|ỉ|ĩ|ị/gi, 'i');
+    slug = slug.replace(/ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ/gi, 'o');
+    slug = slug.replace(/ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự/gi, 'u');
+    slug = slug.replace(/ý|ỳ|ỷ|ỹ|ỵ/gi, 'y');
+    slug = slug.replace(/đ/gi, 'd');
+    //Xóa các ký tự đặt biệt
+    slug = slug.replace(/\`|\~|\!|\@|\#|\||\$|\%|\^|\&|\*|\(|\)|\+|\=|\,|\.|\/|\?|\>|\<|\'|\"|\:|\;|_/gi, '');
+    //Đổi khoảng trắng thành ký tự gạch ngang
+    slug = slug.replace(/ /gi, "-");
+    //Đổi nhiều ký tự gạch ngang liên tiếp thành 1 ký tự gạch ngang
+    //Phòng trường hợp người nhập vào quá nhiều ký tự trắng
+    slug = slug.replace(/\-\-\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-\-/gi, '-');
+    slug = slug.replace(/\-\-/gi, '-');
+    //Xóa các ký tự gạch ngang ở đầu và cuối
+    slug = '@' + slug + '@';
+    slug = slug.replace(/\@\-|\-\@|\@/gi, '');
+    //In slug ra textbox có id “slug”
+    document.getElementById('slugPostEdit' + id).value = slug;
+}
+
 function readURL(input) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
@@ -41,10 +72,24 @@ function readURL(input) {
     }
 }
 
+function readURL_2(input, id) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            $('#preview-image-before-edit-posts' + id).attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
 $("#imageCoverPostAdd").change(function() {
     readURL(this);
 });
-
+$(".imagePostsEdit").change(function() {
+    var id = $(this).data("id");
+    readURL_2(this, id);
+})
 $(document).on("submit", "#form-add-post", function(e) {
     e.preventDefault();
     if ($('#titlePostAdd').val() == "") {
@@ -206,4 +251,8 @@ $(document).on("submit", "#form-add-post", function(e) {
         })
     }
 
+})
+$(document).on("submit", "#form-update-post", function(e) {
+    e.preventDefault();
+    alert("Chưa làm !");
 })
--- resources/views/posts/all.blade.php
+++ resources/views/posts/all.blade.php
@@ -2,7 +2,10 @@
 @section('title','Quản lý bài viết')
 @section('main-container')
 <?php
+
 use Carbon\Carbon;
+
+Carbon::setLocale('vi');
 ?>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
 <link rel="stylesheet" href="admin/script/tag-input/tagsinput.css">
@@ -106,16 +109,83 @@ use Carbon\Carbon;
                 <tbody id="resultAllPosts">
                     @foreach($allPosts as $row)
                     <tr>
-                        <td>{{ $row->titlePosts }}</td>
+                        <td>{!! $row->titlePosts !!}</td>
                         <td class="td-image-posts">
-                            <div><img src="http://127.0.0.1:3000/<?php echo "images/posts/".$row->imagePosts ?>" alt=""></div>
+                            <div><img src="http://127.0.0.1:3000/<?php echo "images/posts/" . $row->imagePosts ?>" alt=""></div>
                         </td>
                         <td>{!! $row->summaryPosts !!}</td>
-                        <td>12:30 28/10/2022</td>
+                        <td><?php echo Carbon::parse($row->created_at)->format('h:i') . " " . Carbon::parse($row->created_at)->format('d/m') . "/" . Carbon::parse($row->created_at)->year . " (" . Carbon::parse($row->created_at)->diffForHumans() . ")" ?></td>
                         <td>
                             <div class="btn-group" role="group" aria-label="Basic example">
-                                <button type="button" class="btn btn-warning"><i class="fas fa-pen-alt"></i></button>
+                                <button type="button" class="btn btn-warning" data-toggle="modal" data-target=".box-edit-post-modal{{ $row->id }}"><i class="fas fa-pen-alt"></i></button>
                                 <button type="button" class="btn btn-danger"><i class="fas fa-trash-alt"></i></button>
+                            </div>
+                            <div class="modal fade box-edit-post-modal{{ $row->id }}" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-lg">
+                                    <div class="modal-content p-4">
+                                        <div class="h4">Chỉnh sửa "{!! $row->titlePosts !!}"</div>
+                                        <hr>
+                                        <form id="form-update-post" enctype="multipart/form-data">
+                                            <div class="form-group">
+                                                <label for="">Tiêu đề</label>
+                                                <input type="hidden" name="id" value="{{ $row->id }}">
+                                                <input onkeyup="ChangeToSlugEditPost(this)" type="text" data-id="{{ $row->id }}" name="title" class="form-control titlePostEdit" value="{!! $row->titlePosts !!}" id="titlePostEdit{{ $row->id }}" placeholder="Nhập tiêu đề bài viết">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Slug</label>
+                                                <input onkeyup="ChangeToSlugEditPost(this)" type="text" data-id="{{ $row->id }}" name="slug" class="form-control slugPostEdit" value="{{ $row->slugPosts }}" id="slugPostEdit{{ $row->id }}" placeholder="Slug tiêu đề">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="">Từ khóa tìm kiếm</label>
+                                                <input data-role="tagsinput" type="text" class="form-control tagsPostEdit" value="{{ $row->tagsPosts }}" name="tags" id="tagsPostEdit" placeholder="Nhập từ khóa tìm kiếm">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Tóm tắt bài viết</label>
+                                                <textarea class="summaryPostEdit" name="summary" id="summaryPostEdit{{ $row->id }}" cols="30" rows="10">{!! $row->summaryPosts !!}</textarea>
+                                                <script>
+                                                    CKEDITOR.replace('summaryPostEdit{{ $row->id }}', {
+                                                        height: 400,
+                                                    });
+                                                </script>
+                                            </div>
+                                            <img src="http://127.0.0.1:3000/<?php echo "images/posts/" . $row->imagePosts ?>" id="preview-image-before-edit-posts{{ $row->id }}" width="200px" height="150px">
+                                            <div class="input-group mb-3">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text" id="inputGroupFileAddon01">Ảnh bìa bài viết</span>
+                                                </div>
+                                                <div class="custom-file">
+                                                    <input data-id="{{ $row->id }}" type="file" class="custom-file-input imagePostsEdit" name="file-image" id="imagePostsEdit{{ $row->id }}" aria-describedby="inputGroupFileAddon01">
+                                                    <label class="custom-file-label" for="imagePostsEdit{{ $row->id }}">Chọn tệp</label>
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Thể loại bài viết</label>
+                                                <select id="selectCatePostEdit" name="cate" class="custom-select selectCatePostEdit">
+                                                    @foreach($catePosts as $item)
+                                                    <option {{ $show = ($item->id == $row->idcatePosts) ? 'selected' : '' }} value="{{ $item->id }}">{{ $item->nameCatePosts }}</option>
+                                                    @endforeach
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Trạng thái</label>
+                                                <select name="status" class="custom-select">
+                                                    <option {{ $show = ($row->statusPosts == 1) ? 'selected' : '' }} value="1">Lưu hành</option>
+                                                    <option {{ $show = ($row->statusPosts == 2) ? 'selected' : '' }} value="2">Ngừng lưu hành</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="exampleInputPassword1">Nội dung bài viết</label>
+                                                <textarea class="contentPostEdit" name="content" id="contentPostEdit{{ $row->id }}" cols="30" rows="10">{!! $row->contentPosts !!}</textarea>
+                                                <script>
+                                                    CKEDITOR.replace('contentPostEdit{{ $row->id }}', {
+                                                        height: 400,
+                                                    });
+                                                </script>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">[<i class="fas fa-save"></i>] Lưu bài viết</button>
+                                        </form>
+                                    </div>
+                                </div>
                             </div>
                         </td>
                     </tr>
